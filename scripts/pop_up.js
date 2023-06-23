dd10af01512e4b6d1354f16debfae5232d247776
Elvira-dd <elvira.6.galieva@yandex.ru>
2023-06-23 07:39:27 +0300

    Co-authored-by: BigBanana <biggban@users.noreply.github.com>

--- scripts/pop_up.js
+++ scripts/pop_up.js
@@ -1,35 +1,13 @@
-ball.onmousedown = function (event) {
-  let shiftX = event.clientX - ball.getBoundingClientRect().left;
-  let shiftY = event.clientY - ball.getBoundingClientRect().top;
+// let openButton = document.getElementById("aboutus_button_pop_up");
+// let popup = document.getElementById("about_pop_up_1_i");
+// openButton.addEventListener("click", function () {
+//   // popup.style.display = "block";
+//   console.log("hello");
+// });
 
-  ball.style.position = "absolute";
-  ball.style.zIndex = 1000;
-  document.body.append(ball);
-
-  moveAt(event.pageX, event.pageY);
-
-  // переносит мяч на координаты (pageX, pageY),
-  // дополнительно учитывая изначальный сдвиг относительно указателя мыши
-  function moveAt(pageX, pageY) {
-    ball.style.left = pageX - shiftX + "px";
-    ball.style.top = pageY - shiftY + "px";
-  }
-
-  function onMouseMove(event) {
-    moveAt(event.pageX, event.pageY);
-  }
-
-  // передвигаем мяч при событии mousemove
-  document.addEventListener("mousemove", onMouseMove);
-
-  // отпустить мяч, удалить ненужные обработчики
-  ball.onmouseup = function () {
-    document.removeEventListener("mousemove", onMouseMove);
-    ball.onmouseup = null;
-  };
-};
-
-ball.ondragstart = function () {
-  return false;
-};
-// Это все надо настраивать
+let openButton = document.getElementById("button_poster_popup");
+let popup = document.getElementById("about_pop_up_1_i");
+openButton.addEventListener("click", () => {
+  popup.style.display = "block";
+  console.log("hello");
+});
